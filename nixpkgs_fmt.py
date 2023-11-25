fe2fa096d18e9902219f9ef01ffd6b54bff88bbd
CrepeGoat <studentoflife96@gmail.com>
2023-11-24 20:53:29 -0700

    added format-on-save feature

--- nixpkgs_fmt.py
+++ nixpkgs_fmt.py
@@ -9,8 +9,8 @@ package_name = "nixpkgs-fmt"
 class NixpkgsFmtCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         # encoding = self.view.encoding()
-        settings = sublime.load_settings(package_name + ".sublime-settings")
-        fmt_command: list = settings.get("fmt_command")
+        package_settings = sublime.load_settings(package_name + ".sublime-settings")
+        fmt_command: list = package_settings.get("fmt_command")
 
         full_region = sublime.Region(0, self.view.size())
         original_text = self.view.substr(full_region)
@@ -39,3 +39,20 @@ class NixpkgsFmtCommand(sublime_plugin.TextCommand):
         return syntax_lower.endswith("nix.tmlanguage") or syntax_lower.endswith(
             "nix.sublime-syntax"
         )
+
+
+class NixpkgsFmtOnSaveEventListener(sublime_plugin.ViewEventListener):
+    def is_applicable(settings):
+        syntax_lower = settings.get("syntax").lower()
+        is_applicable_syntax = syntax_lower.endswith(
+            "nix.tmlanguage"
+        ) or syntax_lower.endswith("nix.sublime-syntax")
+
+        package_settings = sublime.load_settings(package_name + ".sublime-settings")
+        return package_settings.get("trigger_on_save") and is_applicable_syntax
+
+    def applies_to_primary_view_only():
+        return True
+
+    def on_pre_save(self):
+        self.view.run_command("nixpkgs_fmt")
